d3d346352400887af7db92ea7731d369e906028c
RFOyashiro <kurt.savio+git@hotmail.fr>
2026-05-13 21:17:08 +0200

    0.9 - Add locations for other rewards on all quests with multiple rewards

--- locations/makeRef.py
+++ locations/makeRef.py
@@ -1,7 +1,7 @@
 import jstyleson
 import json
 
-f = open('locations/botanics.json')
+f = open('locations/azureArchipelago.json')
 text = f.read()
 f.close()
 
@@ -9,10 +9,11 @@ jData = jstyleson.loads(text)
 jOut = '[\n'
 
 for jName in jData[0]["children"]:
-    Name = jName["name"]
-    for jSection in jName["sections"]:
-        Section = jSection["name"]
-        jOut += '                    {\n                        "ref": "' + Name + '/' + Section + '",\n                        "name": "' + Name + ' - ' + Section + '"\n                    },\n'
+    if "name" in jName:
+        Name = jName["name"]
+        for jSection in jName["sections"]:
+            Section = jSection["name"]
+            jOut += '                    {\n                        "ref": "' + Name + '/' + Section + '",\n                        "name": "' + Name + '\\n' + Section + '"\n                    },\n'
 
 jOut += ']'
 
